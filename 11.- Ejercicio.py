35419ee105fb7e01641ff0456a8ee0bd31c7f63b
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2022-09-27 12:24:55 -0300

    Classe 27-09-2022 Parte 2

--- 11.- Ejercicio.py
+++ 11.- Ejercicio.py
@@ -2,8 +2,23 @@
 
 # Luz Azul
 # Ana
-# Yo hago Yoha hoy
+# Yo hago Yoga hoy
 # Anita lava la tina
 
 # Escribir un programa que identifique si una palabra es palíndromo o no
 
+def palindromo(palabra):
+    palabra = palabra.replace(' ', '')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+
+    if palabra == palabra_invertida:
+        return True
+    else:
+        return False
+
+palabra = palindromo(input("Ingrese frase: "))
+if palabra:
+    print("Si es un palindromo")
+else:
+    print("No es palindormo")
